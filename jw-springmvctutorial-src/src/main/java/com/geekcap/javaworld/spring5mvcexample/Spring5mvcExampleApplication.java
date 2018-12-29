8c73584eae520bbd704ae3fdbf123268a210ec3b
said senhadji <elsenhad@gmail.com>
2018-12-29 11:19:06 -0500

    initial commit of the Demo

---
+++ jw-springmvctutorial-src/src/main/java/com/geekcap/javaworld/spring5mvcexample/Spring5mvcExampleApplication.java
@@ -0,0 +1,22 @@
+package com.geekcap.javaworld.spring5mvcexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.ConcurrentModificationException;
+
+@SpringBootApplication
+public class Spring5mvcExampleApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(Spring5mvcExampleApplication.class, args);
+
+
+
+
+
+
+	}
+}
